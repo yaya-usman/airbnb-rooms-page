5df98fcf8ed32aa7422c41cff9ca75911f4acf3a
yaya-usman <yayaazeez222@gmail.com>
2021-12-08 18:54:19 +0300

    navbar layout complete

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,10 +7,30 @@ const Navbar = () => {
     <div className={styles.navbar}>
       <div className={styles.navbarWrapper}>
         <div className={styles.left}>
-            <img src={logo} alt="logo"  />
+          <img src={logo} alt="logo" />
         </div>
-        <div className={styles.center}>search bar</div>
-        <div className={styles.right}>Menu</div>
+        <div className={styles.center}>
+          <div className={styles.searchbar}>
+            <span>Start your search</span>
+            <span className={styles.searchIcon}>
+              <i className="fas fa-search"></i>
+            </span>
+          </div>
+        </div>
+        <div className={styles.right}>
+          <button>Become a Host</button>
+          <div className={styles.globe}>
+            <i className="fas fa-globe"></i>
+          </div>
+        </div>
+        <div className={styles.menu}>
+          <span>
+            <i className="fas fa-bars"></i>
+          </span>
+          <span>
+            <i className="fas fa-user-circle"></i>
+          </span>
+        </div>
       </div>
     </div>
   );
