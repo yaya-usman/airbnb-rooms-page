a7fc2ee3e61787579fb5d3665901adbbc1edeff3
yayausma <yayausma@fit.cvut.cz>
2021-12-09 03:26:02 +0300

    gallery section layout update

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import React from "react";
 import styles from "../styles/Gallery.module.css";
 
-
 const Gallery = () => {
-    return (
+  return (
     <div className={styles.gallery}>
-        Gallery
+      <div className={styles.item1}>
+        <img
+          src="https://a0.muscache.com/im/pictures/a4c62997-9c90-44ff-9fd6-76b289345456.jpg?im_w=960"
+          alt="img1"
+        />
+      </div>
+      <div className={styles.item2}>
+        <img
+          src="https://a0.muscache.com/im/pictures/90550d13-1577-46c4-be38-2ba4c4eca47c.jpg?im_w=720"
+          alt="img2"
+        />
+      </div>
+      <div className={styles.item3}>
+        <img
+          src="https://a0.muscache.com/im/pictures/32d5e7e6-f091-4851-8302-f3b8f5342a0f.jpg?im_w=720"
+          alt="img3"
+        />
+      </div>
+      <div className={styles.item4}>
+        <img
+          src="https://a0.muscache.com/im/pictures/c1271873-9c68-4257-be96-80f136830aa9.jpg?im_w=720"
+          alt="img4"
+        />
+      </div>
+      <div className={styles.item5}>
+        <img
+          src="https://a0.muscache.com/im/pictures/644cae9d-0270-47c1-ad48-dcb7462f9836.jpg?im_w=720"
+          alt="img5"
+        />
+        <button className={styles.allBtn}>
+          <i className="fas fa-braille"></i>Show all photos
+        </button>
+      </div>
     </div>
-    )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,40 @@
-import React from 'react'
+import React from "react";
 import styles from "../styles/Header.module.css";
-import {Gallery} from '../components'
+import { Gallery } from "../components";
 
 const Header = () => {
-    return (
-        <div className={styles.header}>
-            <div className={styles.headingsContainer}>
-                <h3>Ağaoğlu Dağevleri</h3>
-                <div className={styles.details}>
-                    <span className={styles.review}><i className="fas fa-star"></i>4.88 <a href="#">(52 Reviews)</a></span>
-                    <a href="#">Sapanca,Sakarya,Turkey</a>
-                    <div className={styles.icons}>
-                        <span><a href="#"><i className="fas fa-external-link-alt"></i>Share</a></span>
-                        <span><a href="#"><i className="far fa-heart"></i>Save</a></span>
-                    </div>
-                </div>
-                <Gallery />
-
+  return (
+    <div className={styles.header}>
+      <div className={styles.headerWrapper}>
+        <div className={styles.headingsContainer}>
+          <h3>Ağaoğlu Dağevleri</h3>
+          <div className={styles.details}>
+            {/* left */}
+            <div className={styles.left}>
+              <span className={styles.review}>
+                <i className="fas fa-star" style = {{color: "#FF385C"}}></i>4.88 <a href="#">(52 reviews)</a>
+              </span>
+              <a href="#">Sapanca,Sakarya,Turkey</a>
             </div>
+            {/* right */}
+            <div className={styles.right}>
+              <span>
+                <a href="#">
+                  <i className="fas fa-external-link-alt"></i>Share
+                </a>
+              </span>
+              <span>
+                <a href="#">
+                  <i className="far fa-heart"></i>Save
+                </a>
+              </span>
+            </div>
+          </div>
+          <Gallery />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
